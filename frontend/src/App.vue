c30a2825ea8abe98cf60734a8f81404c280e521a
Darren Alexis <73722835+nonealall@users.noreply.github.com>
2023-10-29 20:04:35 -0400

    Your commit message

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Sign Up</router-link>
+    <router-link to="/signup">Sign Up</router-link>|
+    <router-link to="/signup">Sign Up</router-link> |
+    <router-link to="/dashboard">Dashboard</router-link>
   </nav>
   <router-view/>
 </template>
