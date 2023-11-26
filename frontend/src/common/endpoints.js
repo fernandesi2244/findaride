626602a636445d318796c715fe3e1cbee6b30aca
Ian Fernandes <fernandesi2244@gmail.com>
2023-11-26 14:15:15 -0600

    Fix endpoint route

--- frontend/src/common/endpoints.js
+++ frontend/src/common/endpoints.js
@@ -8,7 +8,7 @@ const endpoints = {
   activate: `${authEndpoint}/activate/`,
   me: `${authEndpoint}/me/`,
 
-  tripRequest: `${apiEndpoint}/trip-requests/`,
+  tripRequest: `${apiEndpoint}/trip-request/`,
   userTrips: `${apiEndpoint}/user-trips/`,
   deleteTripRequest: `${apiEndpoint}/delete-trip-request/`,
   confirmationRequests: `${apiEndpoint}/confirmation-requests/`,
