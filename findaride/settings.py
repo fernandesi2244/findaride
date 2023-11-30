b07658097bfce8db9ffab63bbe378506f738e5e9
koheisanno <kohei.sanno@gmail.com>
2023-11-30 14:27:05 -0500

    emails with service account

--- findaride/settings.py
+++ findaride/settings.py
@@ -178,7 +178,7 @@ CAS_REDIRECT_URL = "/dashboard/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.princeton.edu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_ID') 
