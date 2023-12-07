fa5902517c8bbae888b021a2de6ca78423487f7c
koheisanno <kohei.sanno@gmail.com>
2023-12-07 18:45:37 -0500

    test

--- api/views.py
+++ api/views.py
@@ -179,18 +179,6 @@ class TripRequestAPIView(views.APIView):
                 num_luggage_bags=trip_request.num_luggage_bags,
             )
 
-            user_stats = trip_request.user.user_stats
-        
-            user_stats.number_trips += 1
-            from_coord = (trip.departure_location.latitude, trip.departure_location.longitude)
-            to_coord = (trip.arrival_location.latitude, trip.arrival_location.longitude)
-            user_stats.miles_ridden += int(geodesic(from_coord, to_coord).miles)
-        
-            for participant in trip.participant_list.all():
-                user_stats.past_riders.add(participant)
-
-            user_stats.save()
-
             trip_request.delete()
 
             return Response({'message': 'Trip created'}, status=status.HTTP_200_OK)
