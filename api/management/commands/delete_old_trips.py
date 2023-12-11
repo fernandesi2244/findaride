f9e8fbe84948808d398d1f13009e5f4e313eaee0
koheisanno <kohei.sanno@gmail.com>
2023-12-11 16:00:21 -0500

    background workers

---
+++ api/management/commands/delete_old_trips.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from api.models import TripRequest, Trip
+import datetime
+from geopy.distance import geodesic
+from django.utils.timezone import make_aware
+
+class Command(BaseCommand):
+    help = 'Delete old trips'
+
+    def handle(self, *args, **kwargs):
+        old_trips = Trip.objects.filter(latest_departure_time__lte=make_aware(datetime.datetime.utcnow()))
+        for trip in old_trips.all():
+            for participant in trip.participant_list.all():
+                user_stats = participant.user_stats
+
+                if user_stats:
+                    user_stats.number_trips += 1
+                    from_coord = (trip.departure_location.latitude, trip.departure_location.longitude)
+                    to_coord = (trip.arrival_location.latitude, trip.arrival_location.longitude)
+                    user_stats.miles_ridden += int(geodesic(from_coord, to_coord).miles)
+                    for participant in trip.participant_list.all():
+                        user_stats.past_riders.add(participant)
+
+                    user_stats.save()
+
+        
+        self.stdout.write(f'deleting {old_trips.count()} trips')
+        old_trips.delete()
+
+        old_trip_requests = TripRequest.objects.filter(latest_departure_time__lte=make_aware(datetime.datetime.utcnow()))
+
+        self.stdout.write(f'deleting {old_trip_requests.count()} trip requests')
+        old_trip_requests.delete()
