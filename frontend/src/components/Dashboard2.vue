83afe32d83f5bbac6b60e89d9c370f342eba679e
Dylan Li <dylanli859@gmail.com>
2023-12-12 19:26:13 -0500

    request join trips

--- frontend/src/components/Dashboard2.vue
+++ frontend/src/components/Dashboard2.vue
@@ -12,7 +12,7 @@
     <ConfirmDialogue ref="confirmDialogue"> </ConfirmDialogue>
     <JoinTripsModal @joinSelectedTrips="joinSelectedTrips" @refreshTrips="refreshData" 
                     :trips="selectedTrips" ref="joinTripsRef"></JoinTripsModal>
-
+    
   <div class="container-xl" style="row-gap: 20px;">
     <div class="narrow-container">
       <div class="d-flex justify-content-between">
@@ -34,7 +34,7 @@
           <!-- <button v-tooltip="'Would you like to create a trip?'" id="add-trip-btn" @click="addManualTripRequest"
             class="btn btn-primary">Create a new trip</button> -->
             <div class="flex mt-4">
-                <button @click="toggleShowJoinTripsModal" :disabled="false" class="btn btn-primary mt-4">
+                <button @click="toggleShowJoinTripsModal" :disabled="noTripsSelected" class="btn btn-primary mt-4">
                     Request to join
                 </button>
                 <h3 style="margin: auto; padding-top: 15px;">Trip Matches</h3>
@@ -43,7 +43,6 @@
                 </div>
             </div>
           
-
           <v-data-table v-model="selectedTrips" :headers="headers" :items="filteredTrips" item-key="id" show-select>
             <template v-slot:item.latest_departure_time="{ item }">
               <div class="text-start">{{ getDateOrRange(item.earliest_departure_time, item.latest_departure_time) }}</div>
@@ -67,7 +66,6 @@
           <ManageTripsTab :trips="userTrips" :tripRequests="tripRequests" :userID="user.id"></ManageTripsTab>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -103,6 +101,7 @@ const confirmDialogue = ref(null);
 const joinTripsRef = ref(null);
 const selectedTrips = ref([]);
 const showJoinTripsModal = ref(false);
+const createTripModalRef = ref(null);
 
 const headers = ref([
   {
@@ -137,21 +136,6 @@ const headers = ref([
   },
 ])
 
-// display vars
-// const tripComment = ref("");
-// const addTripModal = ref(null)
-// const confirmDialogue = ref(null);
-// const selectedTrips = ref([]);
-// const createTripData = ref({});
-const createTripModalRef = ref(null);
-
-function toggleCreateTripModal() {
-    console.log(addTripModal.value.trip)
-    createTripModalRef
-    // createTripData.value = addTripModal.value.getData()
-    createTripModalRef.value.show()
-}
-
 function toggleTripModal() {
   addTripModal.value.show();
 }
@@ -160,20 +144,6 @@ function toggleHelp() {
   if (tripHelpModalRef.value) {
     tripHelpModalRef.value.show();
   }
-}
-
-function getTripFormData(trip) {
-    // console.log(addTripModal.value.trip)
-    return {
-        from: trip.from,
-        to: trip.to,
-        earliestDepartureTime: trip.earliestDepartureTime,
-        latestDepartureTime: trip.latestDepartureTime
-    }
-}
-
-async function createNewTrip() {
-
 }
 
 async function refreshData() {
@@ -329,6 +299,10 @@ async function removeTripRequest(id) {
 }
 
 function toggleShowJoinTripsModal() {
+    if (noTripsSelected()) {
+        alert("Please select at least one trip.");
+        return;
+    }
     joinTripsRef.value.show()
 }
 
@@ -366,6 +340,25 @@ async function joinSelectedTrips() {
 </script>
   
 <style>
+.tab-content {
+    background-color: white;
+    border: solid;
+    border-width: thin;
+    /* border-left-width: thin;
+    border-right-width: thin;
+    border-top-width: 0;
+    border-bottom-width: thin; */
+    border-top-right-radius: var(--bs-border-radius);
+    border-bottom-left-radius: var(--bs-border-radius);
+    border-bottom-right-radius: var(--bs-border-radius);
+    border-color: var(--bs-border-color);
+
+}
+
+.nav-tabs {
+    border-bottom-width: 0;
+}
+
 .nav-link {
   --bs-nav-link-color: #3B3B3B;
   --bs-nav-pills-link-active-color: #fff;
---
+++ frontend/src/components/JoinTripsModal.vue
@@ -0,0 +1,239 @@
+<template>
+    <popup-modal ref="popup" medium>
+      <div class="modal-content" style="text-align: left;">
+        <div class="modal-body">
+          <form @submit.prevent="submit" class="form-container">
+            <h5 class="nickname">
+                <label class="label" for="nickname">Nickname:</label>
+                <input class="input" id="nickname" v-model="myData.nickname" type="text" placeholder="e.g. Spring break" maxlength="255" required>
+            </h5>
+            <div class="luggage">
+                    <label class="text-start" for="luggage-count">Number of luggage bags:</label>
+                    <input id="luggage-count" v-model.number="myData.luggageCount" type="number" min="0" required />
+                </div>
+            <div class="form-group">
+              <label class="text-start" for="comments">Additional comments:</label>
+              <textarea id="comments" v-model="myData.comment" placeholder="Any special accommodations, considerations, etc."
+                maxlength="255"></textarea>
+            </div>
+            
+            
+            Request to join <b>{{ trips.length }}</b> trip(s)
+            <hr>
+            <div class="create-trip-footer">
+                <button type="button" @click="_close" class="btn cancel-btn">Close</button>
+              <button type="submit" class="btn primary">Create trip request</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </popup-modal>
+  </template>
+  
+  <script>
+  import PopupModal from "./PopupModal.vue";
+  
+  let fromLocationWasSelected = false;
+  let toLocationWasSelected = false;
+  
+  export default {
+    name: "AddTripModal",
+    components: {
+      PopupModal,
+    },
+    props: ['trips'],
+
+    data() {
+      return {
+        myData: {
+            nickname: '',
+            luggageCount: 0,
+            comment: '',
+        },
+      }
+    },
+    methods: {
+      show() {
+        console.log("HERE")
+        this.$refs.popup.open();
+      },
+      _close() {
+        this.$refs.popup.close();
+      },
+      resetForm() {
+        this.myData.nickname = ''
+        this.myData.luggageCount = 0;
+        this.myData.comment = '';
+      },
+      submit() {
+        this.$emit('joinSelectedTrips', { ...this.trips } , this.myData );
+        this.$emit('refreshTrips');
+        this.resetForm();
+        this._close();
+      }
+    }
+  }
+  </script>
+  <style>
+  :root {
+    --primary-color: #007bff;
+    --secondary-color: #6c757d;
+    --input-border-color: #ccc;
+    --hover-opacity: 0.9;
+  }
+  
+  
+  .trip-request-form {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    /* left-align all elements */
+    text-align: left;
+  }
+  
+  
+  .trip-request-form h3 {
+    text-align: center;
+    color: #333;
+    margin-bottom: 20px;
+  }
+  
+  
+  /* Style for form groups */
+  .form-group {
+    margin-bottom: 15px;
+    text-align:left;
+  }
+  
+  
+  .form-group label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+    color: #555;
+  }
+  
+  
+  .form-group input,
+  .form-group textarea {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  
+  
+  .form-group textarea {
+    height: 100px;
+    resize: vertical;
+  }
+  
+  
+  /* Style for buttons */
+  .form-actions {
+    text-align: right;
+    padding-top: 10px;
+  }
+  
+  
+  .btn {
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  
+  
+  .btn.primary {
+    background-color: #007bff;
+    color: white;
+  }
+  
+  
+  .btn.cancel-btn {
+    background-color: #6c757d;
+    color: white;
+    margin-left: 10px;
+  }
+  
+  
+  .btn:hover {
+    opacity: 0.9;
+  }
+  
+  
+  /* Additional styles for responsiveness and interactivity */
+  @media (max-width: 768px) {
+    .trip-request-form {
+      padding: 10px;
+    }
+  }
+  
+  
+  input:focus,
+  textarea:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+  
+  .btn {
+    border-radius: 30px;
+  }
+  
+  
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .invalid-location-modal {
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1000;
+    width: 80%;
+    max-width: 400px;
+  }
+  
+  .invalid-location-modal .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 15px;
+  }
+  
+  .invalid-location-modal .modal-header .close-btn {
+    border: none;
+    background: none;
+    font-size: 1.5em;
+    cursor: pointer;
+  }
+  
+  .invalid-location-modal .modal-body p {
+    color: #333;
+  }
+  
+  .invalid-location-modal .modal-footer {
+    text-align: right;
+  }
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+    }
+  </style>
